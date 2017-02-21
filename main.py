14d7feb5a6ae6cdeccfdfaef6871d57da7eea21a
Sulochana Kodituwakku <sulochana.456@live.com>
2017-02-21 11:13:43 +0530

    chec file exists

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 from time import gmtime, strftime
+import os.path
 
 
 def stamp(mode=0):
@@ -19,16 +20,28 @@ def stamp(mode=0):
 def main():
     print stamp(),"Initiating"
     parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--encrypt", help="Embed a message into an image", action='store_true')
+    parser.add_argument("-d", "--decrypt", help="Extract a message from an image", action='store_true')
     parser.add_argument("-o", "--output", help="Output Filename", type=str)
     parser.add_argument("-i", "--input", help="Input Filename", type=str)
     args = parser.parse_args()
 
-    if args.input:
+    if args.encrypt:
         print stamp(), "Entering Steganography Encryption Mode"
-    elif args.output:
+        if args.input:
+            print stamp(),"Searching for the file",args.input
+            if (os.path.isfile(args.input)):
+                print stamp(),"The file",args.input,"Found"
+            else:
+                print stamp(2),"The file", args.input, "was not found"
+                print stamp(),"Exiting program"
+        else:
+            print stamp(2),"No input file is provided"
+            print stamp(),"Exiting Program"
+    elif args.decrypt:
         print stamp(), "Entering Steganography Decryption Mode"
     else:
-        print stamp(2), "Parameters are not provided. Program is terminating"
+        print stamp(2), "Required parameters are not provided. Program is terminating"
 
 
 main()
